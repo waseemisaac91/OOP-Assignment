a460da9ce39c6eb69416ad6e70b0e082e21a7dd8
Mhdarab <mhd.arab@yahoo.com>
2026-09-23 21:55:28 +0200

    changing display info

--- q4_vehicle.py
+++ q4_vehicle.py
@@ -5,17 +5,32 @@ class vehicle:
         self.model = model
         self.year = year
 
+    def display_info(self):
+        print("Make:", self.make)
+        print("Model:", self.model)
+        print("Year:", self.year)
+
 # Child Class 1
 class OffRoadVehicle(vehicle):
     def __init__(self, make, model, year, four_wheel_drive):
         super().__init__(make, model, year)
         self.four_wheel_drive = four_wheel_drive
 
+    def display_info(self):
+        print("\nOff-Road Vehicle:")
+        super().display_info()
+        print("Four Wheel Drive:", self.four_wheel_drive)
+
 # Child Class 2
 class SportsCar(vehicle):
     def __init__(self, make, model, year, max_speed):
         super().__init__(make, model, year)
         self.max_speed = max_speed
+
+    def display_info(self):
+        print("\nSports Car:")
+        super().display_info()
+        print("Max Speed:", self.max_speed, "km/h")
 
 # Create objects
 vehicle = vehicle("Toyota", "Corolla", 2022)
@@ -24,22 +39,8 @@ offroad = OffRoadVehicle("Jeep", "Wrangler", 2023, True)
 
 sports_car = SportsCar("Ferrari", "488", 2024, 330)
 
-# Display Vehicle properties
+# Display information
 print("Vehicle:")
-print("Make:", vehicle.make)
-print("Model:", vehicle.model)
-print("Year:", vehicle.year)
-
-# Display OffRoadVehicle properties
-print("\nOff-Road Vehicle:")
-print("Make:", offroad.make)
-print("Model:", offroad.model)
-print("Year:", offroad.year)
-print("Four Wheel Drive:", offroad.four_wheel_drive)
-
-# Display SportsCar properties
-print("\nSports Car:")
-print("Make:", sports_car.make)
-print("Model:", sports_car.model)
-print("Year:", sports_car.year)
-print("Max Speed:", sports_car.max_speed, "km/h")
+vehicle.display_info()
+offroad.display_info()
+sports_car.display_info()
